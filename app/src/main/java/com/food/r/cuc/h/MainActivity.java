4fdf4010eca66eeb128daf6abce53e29976ba271
piyushvarshney <piyushvarshney@outlook.com>
2018-03-26 23:14:27 +0000

    new updated, collaborated

--- app/src/main/java/com/food/r/cuc/h/MainActivity.java
+++ app/src/main/java/com/food/r/cuc/h/MainActivity.java
@@ -1,7 +1,9 @@
 package com.food.r.cuc.h;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,10 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
+    public void user(View view)
+    {
+        Intent intent = new Intent(this,OTP.class);
+        startActivity(intent);
+    }
 
-
+    public void ngo(View view)
+    {
+        Intent intent = new Intent(this,SignUpNGOActivity.class);
+        startActivity(intent);
+    }
 
 
 }
