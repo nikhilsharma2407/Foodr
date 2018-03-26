83cf4ccfc1727d97b6288e50df970be53f5ca513
Nikhil <nikhil.sharma2407@gmail.com>
2018-03-26 11:30:01 +0000

    oTP activity 1
    Signed-off-by: Nikhil <nikhil.sharma2407@gmail.com>

--- app/src/main/java/com/food/r/cuc/h/MapActivity.java
+++ app/src/main/java/com/food/r/cuc/h/MapActivity.java
@@ -1,8 +1,26 @@
 package com.food.r.cuc.h;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 public class MapActivity extends AppCompatActivity {
 
     @Override
